ac72eef36d673b8b4c3ac92e9309df701e06874f
Huanghansheng <houqicheng@hotmail.com>
2019-09-05 21:46:43 +0800

    update Main() method

--- AdapterSample/Program.cs
+++ AdapterSample/Program.cs
@@ -16,7 +16,43 @@ namespace AdapterSample
             string adapterType = ConfigurationManager.AppSettings["adapter"];
             operation = (ScoreOperation)Assembly.Load("AdapterSample").CreateInstance(adapterType);
 
+            int[] scores = { 84, 76, 50, 69, 90, 91, 88, 96 };
+            int[] result;
+            int score;
 
+            Console.WriteLine("成绩排序结果");
+            result = operation.Sort(scores);
+
+            //遍历输出成绩
+            foreach (int i in result)
+            {
+                Console.Write(i + ",");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("查找成绩90:");
+            score = operation.Search(result, 90);
+
+            if (score != -1)
+            {
+                Console.WriteLine("找到成绩90");
+            }
+            else
+            {
+                Console.WriteLine("没有找到成绩90。");
+            }
+            Console.WriteLine("查找成绩92：");
+            score = operation.Search(result, 92);
+
+            if (score != -1)
+            {
+                Console.WriteLine("找到成绩92.");
+            }
+            else
+            {
+                Console.WriteLine("没有找到成绩92.");
+            }
+            Console.Read();
         }
     }
 }
